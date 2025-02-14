a4ec7a151af12578e03ac6fae27cc1d64113743e
Taher Magdy <Taher.Magdy@nahdetmisr.com>
2025-02-14 19:25:24 +0200

    uoy

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,18 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class NewTest {
+    @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test3() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+}
