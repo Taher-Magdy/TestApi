3d200826e580c4fd9d42ee89e3073af142fb9cc8
Taher Magdy <Taher.Magdy@nahdetmisr.com>
2025-02-14 20:03:40 +0200

    ll

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,4 +4,37 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class NewTest {
+    @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test1() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+    @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test2() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+    @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test4() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
 }
