95aa65e6a0069f99e66b0c3b924b727a36340302
Taher Magdy <Taher.Magdy@nahdetmisr.com>
2025-02-14 17:50:40 +0200

    first commit

---
+++ src/test/java/TestRepo.java
@@ -0,0 +1,23 @@
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestRepo {
+    static {
+        RestAssured.filters(new AllureRestAssured());
+    }
+    @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test1() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+}
