aac2afd0fd6b6f0c936930ee24a4fdf630a53499
Taher-Magdy <76923461+Taher-Magdy@users.noreply.github.com>
2025-02-14 20:11:30 +0200

    Update NewTest.java

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,4 +4,15 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class NewTest {
+      @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test4() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
 }
