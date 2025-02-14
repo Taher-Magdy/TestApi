da8dc1dc703ffca604cf76a440f553036174a54c
Taher-Magdy <76923461+Taher-Magdy@users.noreply.github.com>
2025-02-14 18:36:20 +0200

    fourth commit from remote

--- src/test/java/TestRepo.java
+++ src/test/java/TestRepo.java
@@ -31,4 +31,15 @@ public class TestRepo {
                 .extract()
                 .response();
     }
+        @Test
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test3() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
 }
