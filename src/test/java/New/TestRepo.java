368b1af3250060cc020a91465bdae149a6502144
Taher Magdy <taher.magdy@nahdetmisr.com>
2025-02-16 16:40:00 +0200

    Neww

--- src/test/java/New/TestRepo.java
+++ src/test/java/New/TestRepo.java
@@ -9,15 +9,6 @@ import static io.restassured.RestAssured.given;
 
 public class TestRepo {
 
-    @Test(priority = 1 , groups = {"New"})
-    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
-    public void test1() {
-        Response response = given()
-                .when()
-                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
-                .then()
-                .extract()
-                .response();
-    }
+
 
 }
