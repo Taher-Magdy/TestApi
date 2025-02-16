ce1d8a4970c6621a90f33d539bd706f66247af0f
Taher Magdy <taher.magdy@nahdetmisr.com>
2025-02-16 19:01:03 +0200

    Newww

--- src/test/java/New/TestRepo.java
+++ src/test/java/New/TestRepo.java
@@ -8,5 +8,16 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class TestRepo {
+    @Test(priority = 1 , groups = {"barcode"})
+    //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
+    public void test3() {
+        Response response = given()
+                .when()
+                .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
 
 }
