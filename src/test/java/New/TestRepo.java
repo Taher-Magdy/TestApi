fb860396fcf51fd5a3da0ad7295799c18e73d814
Taher Magdy <taher.magdy@nahdetmisr.com>
2025-02-16 17:39:06 +0200

    Newww

--- src/test/java/New/TestRepo.java
+++ src/test/java/New/TestRepo.java
@@ -22,7 +22,7 @@ public class TestRepo {
     }
         @Test(priority = 1 , groups = {"barcode"})
     //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
-    public void test3() {
+    public void test4() {
         Response response = given()
                 .when()
                 .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
@@ -33,7 +33,7 @@ public class TestRepo {
     }
         @Test(priority = 1 , groups = {"barcode"})
     //https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users
-    public void test3() {
+    public void test5() {
         Response response = given()
                 .when()
                 .get("https://65c9e1133b05d29307df2a54.mockapi.io/api/v1/users")
